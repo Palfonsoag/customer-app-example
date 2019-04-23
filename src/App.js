a20e626f8b61f80274383ed83ff38d80bc6ddca4
Pedro Alfonso <p.alfonsoag@gmail.com>
2019-04-22 22:25:33 -0400

    Route component initial implementation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 
 class App extends Component {
+  renderHomer = () => <h1>Home</h1>;
+  renderCustomerContainer = () => <h1>Customer Container</h1>;
+  renderCustomerListContainer = () => <h1>Customer List Container</h1>;
+  renderCustomerNewContainer = () => <h1>Customer New Container</h1>;
+
   render() {
     return (
       <Router>
-        <div className="App">
-          <Link to="/customers">{"Customers"}</Link>
-          <br />
-          <Link to="/customers/30000000">{"Customer 30.000.000"}</Link>
+        <div>
+          <Route exact path="/" component={this.renderHomer} />
+          <Route
+            exact
+            path="/customers"
+            component={this.renderCustomerContainer}
+          />
+          <Route
+            exact
+            path="/customers/:dni"
+            component={this.renderCustomerListContainer}
+          />
+          <Route
+            exact
+            path="/customers/new"
+            component={this.renderCustomerNewContainer}
+          />
         </div>
       </Router>
     );
