bf1b1b3cf77fc50767d5f373b6876e6879316b72
Pedro Alfonso <p.alfonsoag@gmail.com>
2019-08-10 16:00:04 -0400

    change CustomerEdit component

--- src/components/CustomerEdit.js
+++ src/components/CustomerEdit.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { reduxForm, Field } from "redux-form";
 import { Prompt } from "react-router-dom";
@@ -29,65 +29,62 @@ const MyField = ({ input, meta, type, label, name }) => (
   </div>
 );
 
-const CustomerEdit = ({
-  name,
-  dni,
-  age,
-  handleSubmit,
-  submitting,
-  onBack,
-  pristine,
-  submitSucceeded
-}) => {
-  return (
-    <div>
-      <h2>{"Customer Edition"}</h2>
-      <form onSubmit={handleSubmit}>
-        <Field
-          name="name"
-          component={MyField}
-          type="text"
-          //validate={isRequired}
-          label="Nombre: "
-        />
+class CustomerEdit extends Component {
+  render() {
+    const {
+      handleSubmit,
+      submitting,
+      onBack,
+      pristine,
+      submitSucceeded
+    } = this.props;
+    return (
+      <div>
+        <h2>{"Customer Edition"}</h2>
+        <form onSubmit={handleSubmit}>
+          <Field
+            name="name"
+            component={MyField}
+            type="text"
+            //validate={isRequired}
+            label="Nombre: "
+          />
 
-        <Field
-          name="dni"
-          component={MyField}
-          type="text"
-          validate={[isNumber]}
-          label="DNI: "
-        />
+          <Field
+            name="dni"
+            component={MyField}
+            type="text"
+            validate={[isNumber]}
+            label="DNI: "
+          />
 
-        <Field
-          name="age"
-          component={MyField}
-          type="number"
-          validate={[isNumber, isRequired]}
-          label="Age: "
-          parse={toNumber}
-        />
-        <CustomerActions>
-          <button type="submit" disabled={pristine || submitting}>
-            Accept
-          </button>
-          <button onClick={onBack} type="button" disabled={submitting}>
-            Cancel
-          </button>
-        </CustomerActions>
-        <Prompt
-          when={!pristine && !submitSucceeded}
-          message="the changes would be lost if you continue"
-        />
-      </form>
-    </div>
-  );
-};
+          <Field
+            name="age"
+            component={MyField}
+            type="number"
+            validate={[isNumber, isRequired]}
+            label="Age: "
+            parse={toNumber}
+          />
+          <CustomerActions>
+            <button type="submit" disabled={pristine || submitting}>
+              Accept
+            </button>
+            <button onClick={onBack} type="button" disabled={submitting}>
+              Cancel
+            </button>
+          </CustomerActions>
+          <Prompt
+            when={!pristine && !submitSucceeded}
+            message="the changes would be lost if you continue"
+          />
+        </form>
+      </div>
+    );
+  }
+}
 
 CustomerEdit.propTypes = {
-  name: PropTypes.string,
-  dni: PropTypes.string,
-  age: PropTypes.number,
   onBack: PropTypes.func.isRequired
 };
 
