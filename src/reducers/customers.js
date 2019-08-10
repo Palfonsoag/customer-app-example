d671509bc5b418b7bbd690172e55ddf268dd8217
Pedro Alfonso <p.alfonsoag@gmail.com>
2019-08-10 15:43:41 -0400

    update reduce

--- src/reducers/customers.js
+++ src/reducers/customers.js
@@ -1,10 +1,25 @@
 import { handleActions } from "redux-actions";
-import { FETCH_CUSTOMERS, CREATE_CUSTOMER } from "../constants";
+import {
+  FETCH_CUSTOMERS,
+  CREATE_CUSTOMER,
+  UPDATE_CUSTOMER
+} from "../constants";
 
 export const customers = handleActions(
   {
     [FETCH_CUSTOMERS]: (state, action) => [...action.payload],
-    [CREATE_CUSTOMER]: (state, action) => [...state, action.payload]
+    [CREATE_CUSTOMER]: (state, action) => [...state, action.payload],
+    [UPDATE_CUSTOMER]: (state, action) => {
+      const { id } = action.payload;
+      const newCustomersList = state.reduce((acc, customer) => {
+        if (customer.id === id) {
+          return [...acc, action.payload];
+        } else {
+          return [...acc, customer];
+        }
+      }, []);
+      return newCustomersList;
+    }
   },
 
   []
